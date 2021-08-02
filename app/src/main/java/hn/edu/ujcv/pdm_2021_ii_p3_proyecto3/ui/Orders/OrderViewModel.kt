fd518320a8294f7bf8abb38be257c18f98d6b6a9
Daniel Alvarez <70470536+djag07907@users.noreply.github.com>
2021-08-01 23:10:11 -0600

    fragment details

--- app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/ui/Orders/OrderViewModel.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/ui/Orders/OrderViewModel.kt
@@ -1,10 +1,10 @@
-package hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.ui.dashboard
+package hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.ui.Orders
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class OrderViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "Fragment de pedidos"
