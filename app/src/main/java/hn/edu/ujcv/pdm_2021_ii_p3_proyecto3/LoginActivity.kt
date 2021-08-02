a4e4b4e46f436dbeeee8ae4b4796bc70f83d97c7
Daniel Alvarez <70470536+djag07907@users.noreply.github.com>
2021-08-01 20:04:56 -0600

    login activity base

--- app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/LoginActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/LoginActivity.kt
@@ -3,9 +3,9 @@ package hn.edu.ujcv.pdm_2021_ii_p3_proyecto3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
