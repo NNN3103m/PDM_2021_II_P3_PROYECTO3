3eadcfb0668f487d1a617b554792b098eb890f83
Daniel Alvarez <70470536+djag07907@users.noreply.github.com>
2021-08-20 19:26:14 -0600

    restapi consumption

--- app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/MainMenuActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/MainMenuActivity.kt
@@ -2,6 +2,7 @@ package hn.edu.ujcv.pdm_2021_ii_p3_proyecto3
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,9 +10,13 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.databinding.ActivityMainMenuBinding
+import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.entities.CustomerDataCollectionItem
 import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.restaurants.RestaurantActivity
 import kotlinx.android.synthetic.main.activity_main_menu.*
 import kotlinx.android.synthetic.main.fragment_home.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainMenuActivity : AppCompatActivity() {
 
@@ -72,5 +77,63 @@ class MainMenuActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+
+// Services Call
+//    Get Customers
+    private fun callServiceGetCustomers(){
+        val customerService: CustomerService = RestEngine.buildService().create(CustomerService::class.java)
+        var result: Call<List<CustomerDataCollectionItem>> = customerService.listCustomers()
+
+        result.enqueue(object : Callback<List<CustomerDataCollectionItem>> {
+            override fun onFailure(call: Call<List<CustomerDataCollectionItem>>, t: Throwable) {
+                Toast.makeText(this@MainMenuActivity, "Error", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(
+                call: Call<List<CustomerDataCollectionItem>>,
+                response: Response<List<CustomerDataCollectionItem>>
+            ) {
+                Toast.makeText(this@MainMenuActivity, "Ok"+response.body()!!.get(0).nombre,Toast.LENGTH_LONG)
+            }
+        })
+    }
+
+//    Get Customer by Id
+
+    private fun callServiceGetCustomer(){
+        val customerService:CustomerService = RestEngine.buildService().create(CustomerService::class.java)
+//        Hacer get de entry, aqui esta en hardcoded/quemado
+        var result: Call<CustomerDataCollectionItem> = customerService.getCustomerById(1)
+
+        result.enqueue(object : Callback<CustomerDataCollectionItem>{
+            override fun onFailure(call: Call<CustomerDataCollectionItem>, t: Throwable) {
+                Toast.makeText(this@MainMenuActivity, "Error", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(
+                call: Call<CustomerDataCollectionItem>,
+                response: Response<CustomerDataCollectionItem>
+            ) {
+                Toast.makeText(this@MainMenuActivity, "OK"+response.body()!!.nombre,Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+//    Post Customer
+    private fun callServicePostCustomer(){
+        val fecha = "2021-8-20"
+    val customerInfo = CustomerDataCollectionItem(null,
+        dni= 888888888888,
+        mobile= 99999999,
+        nombre= "dan",
+        email="dan.mail@domain.com",
+        password="dan.password"
+        )
+
+//        addCustomer(customerInfo){
+//            if (it?)
+//        }
     }
 }
--- app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/entities/CustomerDataCollection.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/entities/CustomerDataCollection.kt
@@ -1,3 +1,12 @@
 package hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.entities
 
-class CustomerDataCollection : ArrayList<CustomerDataCollection>
+class CustomerDataCollection : ArrayList<CustomerDataCollection>()
+
+data class CustomerDataCollectionItem(
+    val id: Long?,
+    val dni:Long,
+    val mobile:Int,
+    val nombre:String,
+    val email: String,
+    val password:String
+)
