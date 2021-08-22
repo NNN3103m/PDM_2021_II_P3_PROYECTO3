4911c608ecf4f1dded3566ae73567fb3d948e682
Daniel Alvarez <70470536+djag07907@users.noreply.github.com>
2021-08-22 05:04:46 -0600

    address

--- app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/entities/AddressDataCollection.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/entities/AddressDataCollection.kt
@@ -3,13 +3,11 @@ package hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.entities
 class AddressDataCollection: ArrayList<AddressDataCollectionItem>()
 
 data class AddressDataCollectionItem(
-    val description:String="",
     val addressId:Long = 0,
-    val country:String="",
-    val department:String="",
-    val city:String="",
-    val postalCode:Int=0,
-    val streetName:String="",
-    val houseNumber:Int,
-    val customerDni:String=""
+    val country:String,
+    val department:String,
+    val city:String,
+    val postalCode:String,
+    val streetName:String,
+    val houseNumber:String,
 )
